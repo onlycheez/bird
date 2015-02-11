6b326f2d06a27200fd9dda80c9d585175289dbcf
Jan Sidlo <sidlojan42@gmail.com>
2015-02-11 14:20:25 +0100

    Creates Windows sysdep sources
    For now they're just copied from unix/ and linux/.

---
+++ sysdep/cf/windows.h
@@ -0,0 +1,19 @@
+/*
+ *	Configuration for Windows systems (tested on Windows 7)
+ *
+ *	(c) 2004 Ondrej Filip <feela@network.cz>
+ *
+ *	Can be freely distributed and used under the terms of the GNU GPL.
+ */
+
+#define CONFIG_AUTO_ROUTES
+#define CONFIG_SELF_CONSCIOUS
+#define CONFIG_MULTIPLE_TABLES
+
+#define CONFIG_SKIP_MC_BIND
+#define CONFIG_NO_IFACE_BIND
+#define CONFIG_USE_HDRINCL
+
+/*
+Link: sysdep/windows
+ */
